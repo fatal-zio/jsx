b5d9fdd5bd6963881702be5d8bd2a3b30dc20b80
fatal-zio <40217128+fatal-zio@users.noreply.github.com>
2019-03-11 15:09:35 -0500

    Added Prettier config and some basic form controls.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
 // Import the React and ReactDOM libraries
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 // Create a react component
 const App = () => {
-  return <div>Hi there!</div>;
+  return (
+    <div>
+      <label class="label" for="name">
+        Enter name:
+      </label>
+      <input id="name" type="text" />
+      <button style={{ backgroundColor: 'blue', color: 'white' }}>
+        Submit
+      </button>
+    </div>
+  );
 };
 
 // Display the component
-ReactDOM.render(<App />, document.querySelector("#root"));
+ReactDOM.render(<App />, document.querySelector('#root'));
