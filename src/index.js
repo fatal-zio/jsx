610b672daf33ddf344a072278e1f3ab97d0125b0
fatal-zio <40217128+fatal-zio@users.noreply.github.com>
2019-03-11 12:29:59 -0500

    Basic hello world example.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 // Import the React and ReactDOM libraries
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-
+import React from "react";
+import ReactDOM from "react-dom";
 
 // Create a react component
-
-
-
+const App = () => {
+  return <div>Hi there!</div>;
+};
 
 // Display the component
+ReactDOM.render(<App />, document.querySelector("#root"));
