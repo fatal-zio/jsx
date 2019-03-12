3e9aebea8d90ce7882c084c3ff9dbe7b88525d33
fatal-zio <40217128+fatal-zio@users.noreply.github.com>
2019-03-11 19:29:26 -0500

    Fixed issue with application icon. Moved some expressions on the demo into variables.

--- src/index.js
+++ src/index.js
@@ -4,14 +4,16 @@ import ReactDOM from 'react-dom';
 
 // Create a react component
 const App = () => {
+  const buttonText = 'Click me!';
+  const labelText = 'Enter name:';
   return (
     <div>
-      <label class="label" for="name">
-        Enter name:
+      <label className="label" htmlFor="name">
+        {labelText}
       </label>
       <input id="name" type="text" />
       <button style={{ backgroundColor: 'blue', color: 'white' }}>
-        Submit
+        {buttonText}
       </button>
     </div>
   );
